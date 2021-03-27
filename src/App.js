719af76bbd17cf7c698cd5dd339e54b4085fa8e7
Prasamsha Sharma <prasamshari@gmail.com>
2021-03-26 17:53:31 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+import PokemonList from "./components/PokemonList";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+
+  const pokeId = () => {
+    const min = Math.ceil(1);
+    const max = Math.floor(151);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  axios.get("https://pokeapi.co/api/v2/pokemon" + pokeId()).then((res) => {
+    setPokemon(res.data.results.map((p) => p.name));
+  });
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await axios(
+  // "https://pokeapi.co/api/v2/pokemon/" + pokeId()
+  //   "https://pokeapi.co/api/v2/pokemon"
+  // );
+  // console.log(result);
+  // setPokemon(`${result.data.sprites.front_default}`);
+  //     setPokemon(`${result.data.results[0].name}`);
+  //   };
+
+  //   fetchData();
+  // }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <img src={pokemon} alt="picpoke" /> */}
+      {/* <div>{pokemon}</div> */}
+      {/* {pokemon.map((pk) => {
+        return (
+          <div key={pk.id}>
+            <img src={pk.sprites.front_default} alt="pokemon" />
+          </div>
+        );
+      })} */}
+      <PokemonList pokemon={pokemon} />
+      {/* <Pagination /> */}
     </div>
   );
 }
---
+++ src/components/Pagination.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pagination = () => {
+  return <div>Next Page</div>;
+};
+
+export default Pagination;
